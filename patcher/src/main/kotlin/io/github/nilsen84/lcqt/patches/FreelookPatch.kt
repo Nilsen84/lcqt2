3b772f4e4e5e30f7c367fa12b21fb5977271fbbd
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-07-28 01:17:48 +0200

    Disallow servers from disabling mods

--- patcher/src/main/kotlin/io/github/nilsen84/lcqt/patches/FreelookPatch.kt
+++ patcher/src/main/kotlin/io/github/nilsen84/lcqt/patches/FreelookPatch.kt
@@ -1,5 +1,6 @@
 package io.github.nilsen84.lcqt.patches
 
+import io.github.nilsen84.bytecode_dsl.asm
 import io.github.nilsen84.lcqt.Patch
 import io.github.nilsen84.lcqt.util.hasCst
 import io.github.nilsen84.lcqt.util.replaceCst
@@ -7,8 +8,20 @@ import org.objectweb.asm.tree.ClassNode
 
 class FreelookPatch : Patch() {
     override fun transform(cn: ClassNode): Boolean {
-        if(!cn.hasCst("modSettings", "pinnedServers")) return false
-        cn.replaceCst("modSettings", "")
-        return true
+        when {
+            cn.hasCst("modSettings", "pinnedServers") -> {
+                cn.replaceCst("modSettings", "")
+                return true
+            }
+            "com/lunarclient/bukkitapi/nethandler/client/LCNetHandlerClient" in cn.interfaces -> {
+                val handleModSettings = cn.methods.find { it.name == "handleModSettings" }!!
+                handleModSettings.instructions = asm {
+                    _return
+                }
+                return true
+            }
+        }
+
+        return false
     }
 }
