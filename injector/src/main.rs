9a87b41db9981d15706d4bf5dc9a9c05e69de410
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-07-28 02:35:38 +0200

    Update launcher search locations

--- injector/src/main.rs
+++ injector/src/main.rs
@@ -3,7 +3,7 @@
 use std::{env, io};
 use std::error::Error;
 use std::net::{Ipv4Addr, TcpListener};
-use std::path::Path;
+use std::path::{Path};
 use std::process::{Command};
 use std::string::String;
 
@@ -18,26 +18,31 @@ fn free_port() -> io::Result<u16> {
 }
 
 fn find_lunar_executable() -> Result<String, String> {
-    let exe = match env::consts::OS {
-        "windows" => env::var("localappdata").or(Err("%localappdata% not defined"))?
-            + r"\Programs\lunarclient\Lunar Client.exe",
-        "macos" => "/Applications/Lunar Client.app/Contents/MacOS/Lunar Client".into(),
-        "linux" => "/usr/bin/lunarclient".into(),
+    let paths: Vec<String> = match env::consts::OS {
+        "windows" => {
+            let localappdata = env::var("localappdata").or(Err("%localappdata% not defined"))?;
+
+            vec![
+                localappdata.clone() + r"\Programs\launcher\Lunar Client.exe",
+                localappdata + r"\Programs\lunarclient\Lunar Client.exe"
+            ]
+        }
+        "macos" => vec!["/Applications/Lunar Client.app/Contents/MacOS/Lunar Client".into()],
+        "linux" => vec!["/usr/bin/lunarclient".into()],
         _ => Err("unsupported os")?
     };
 
-    if !Path::new(&exe).exists() {
-        Err(format!("'{}' does not exist", exe))?
-    }
-
-    Ok(exe)
+    paths.iter()
+        .find(|p| Path::new(p).exists())
+        .ok_or(format!("searched in the following locations: [{}]", paths.join(", ")))
+        .map(|p| p.clone())
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
     let lunar_exe = match env::args().nth(1) {
         Some(arg) => arg,
         _ => find_lunar_executable().map_err(|e|
-            format!("failed to locate lunars launcher, try passing the path to its executable by argument: {}", e)
+            format!("failed to locate lunars launcher: {}", e)
         )?
     };
 
