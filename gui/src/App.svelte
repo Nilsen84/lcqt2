de76f10aea7cf5ed390c385e5c24912e87206b61
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-08-03 00:52:07 +0200

    Fix version in title bar

--- gui/src/App.svelte
+++ gui/src/App.svelte
@@ -70,7 +70,7 @@
 </script>
 
 <svelte:head>
-    <title>Lunar Client Qt v2.0.0</title>
+    <title>Lunar Client Qt {__APP_VERSION__}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600&display=swap" rel="stylesheet">
